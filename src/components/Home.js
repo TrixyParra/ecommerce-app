838e06d95499b77bb918aab0ad06eea43b00e25c
TrixyParra <trixy.parra@gmail.com>
2022-07-31 05:44:39 -0700

    Home.js - changes in layout sections, placed category sections in a div, added/fixed classes on divs and button. App.ccs - started styling categories section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,25 +4,29 @@ export default function Home() {
         <>
             <header className="Home-Header"></header> 
 
-            <div className="Slogan">
-                <h2>Everything you need,<br></br>For You!</h2>
+            <div className="Categories">
+                <h2 className="Category-Title">
+                    Bringing you the latest in a variety of categories to shop from!
+                </h2> 
+                <div className="Category-List">
+                    <div className="Womens">
+                        <h3>Women's Clothing</h3> 
+                    </div> 
+                    <div className="Mens">
+                        <h3>Men's CLothing</h3> 
+                    </div> 
+                    <div className="Jewelry">
+                        <h3>Jewelery</h3> 
+                    </div> 
+                    <div className="Electronics">
+                        <h3>Electronics</h3> 
+                    </div> 
+                </div> 
+                <button className="Shop-Button">Start Shopping</button>
             </div> 
 
-            <div className="Categories">
-                <h2>Bringing you the latest in a variety of categories to shop from!</h2>
-                <div className="Womens">
-                    <h3>Women's Clothing</h3> 
-                </div> 
-                <div className="Mens">
-                    <h3>Men's CLothing</h3> 
-                </div> 
-                <div className="Jewelery">
-                    <h3>Jewelery</h3> 
-                </div> 
-                <div className="Electronics">
-                    <h3>Electronics</h3> 
-                </div> 
-                <button>Start Shopping</button> 
+            <div className="Slogan">
+                <h2>Everything you need,<br></br>For You!</h2>
             </div> 
 
             <div className="Mailing-List">
