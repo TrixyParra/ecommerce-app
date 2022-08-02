41535874564e2dae0057c268df269f65440e078f
TrixyParra <trixy.parra@gmail.com>
2022-08-02 00:38:48 -0700

    Images folder - added Not Found background image. NotFound.js - put all of the content inside another div, made some text changes, and added a class to the link. App.css - added styling to the Not Found page.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-        <div className="Not-Found">
-            <h1>Oops!</h1> 
-            <p>Looks like the page you are looking for cannot be found or no longer exists.</p> 
-            <Link to={"/"}>Back to the Main page</Link> 
-        </div> 
-    ); 
+        <>
+            <div className="Not-Found">
+                <div className="Not-FoundText">
+                    <h1>Oops!</h1> 
+                    <p>The page you are looking for cannot be found or no longer exists.</p> 
+                    <Link to={"/"} className="Link-BackHome">Back to <b>Home</b></Link> 
+                </div> 
+            </div> 
+        </> 
+    );         
 } 
