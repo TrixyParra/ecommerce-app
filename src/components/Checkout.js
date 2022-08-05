578380d1c12be6f14c2a0a65eacebd82cd812ec9
TrixyParra <trixy.parra@gmail.com>
2022-08-05 00:17:02 -0700

    Checkout.js - added checkout form. App.css - styled checkout form.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -10,22 +10,38 @@ export default function Checkout() {
     return (
         <>
             <div className="Cart" style={{ textAlign: "center", padding: "30px" }}>
-                <h2>Checkout</h2>
+                <h2>Checkout</h2> 
+
                 {/* Mapping out the cart */}
                 {cart.map((product) => (
-                <div className="Cart-Items">
+                <div className="Checkout-Items">
                     <img src={product.image} alt="" /> 
                     {/* <div classname="Cart-Details"> */}
                         <p>{product.title}</p> 
                         <p><b>Quantity:</b><br />{product.qty}</p> 
-                        <p><b>Price:</b><br />${product.price}</p> 
+                        <p><b>Price:</b><br />${product.price.toFixed(2)}</p> 
                     {/* </div>  */}
                 </div>
                 ))} 
 
-                <form>
+                <div className="Cart-Total">
+                    <h3>Total:</h3> 
+                    <h3>${cart.reduce((acc, product) => acc + product.qty * product.price, 0).toFixed(2)}</h3> 
+                </div> 
 
-                </form>
+                <form className="Checkout-Form">
+                    <h2>Payment Information</h2> 
+                    <label htmlFor="">First Name</label> 
+                    <input type="text" placeholder="Enter your first name here. . ." required autoComplete="off" /> 
+                    <label htmlFor="">Last Name</label> 
+                    <input type="text" placeholder="Enter your last name here. . ." required autoComplete="off" /> 
+                    <label htmlFor="">Shipping Address</label> 
+                    <input type="text" placeholder="Enter your shipping address here. . ." required autoComplete="off" /> 
+                    <label htmlFor="">Billing Address</label> 
+                    <input type="text" placeholder="Enter your billing address here. . ." required autoComplete="off" /> 
+                    <label htmlFor="">Card Information</label> 
+                    <input type="text" inputMode="numeric" placeholder="XXXX XXXX XXXX XXXX" required autoComplete="off" /> 
+                </form> 
 
                 <Link to="/shop">
                     <button
@@ -34,7 +50,7 @@ export default function Checkout() {
                         }}
                         className="Checkout-Button"
                     >
-                        Order
+                        Place Order
                     </button> 
                 </Link> 
             </div> 
